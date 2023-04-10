fd725ef0374b0ce95dbd79d531b515dec63ca876
logan.lenoir <loganlenoir2006@gmail.com>
2023-04-10 09:52:01 -0600

    Logan Lenoir 4/10/2023 Set up app with atermis color theme. Declared actionmbar in main.

--- app/src/main/java/com/example/haproslaunchapp/MainActivity.java
+++ app/src/main/java/com/example/haproslaunchapp/MainActivity.java
@@ -1,14 +1,17 @@
 package com.example.haproslaunchapp;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    ActionBar main_actionBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        main_actionBar = getSupportActionBar();
     }
 }
