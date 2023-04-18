01d13c05b220d997e39d6b06a3a99975bffb2899
gavin.murdock <gavinbotg@gmai.com>
2023-04-18 08:47:12 -0600

    Media Pages and Internet Access

---
+++ app/src/main/java/com/example/haproslaunchapp/MediaPages.java
@@ -0,0 +1,18 @@
+package com.example.haproslaunchapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MediaPages extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_media_pages);
+    }
+
+
+
+
+}
