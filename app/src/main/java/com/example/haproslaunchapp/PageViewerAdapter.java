a4d47e07df9a5341732b06a4cb9691869f88dcbd
gavin.murdock <gavinbotg@gmai.com>
2023-04-28 08:24:23 -0600

    Worked on json

--- app/src/main/java/com/example/haproslaunchapp/PageViewerAdapter.java
+++ app/src/main/java/com/example/haproslaunchapp/PageViewerAdapter.java
@@ -1,9 +1,18 @@
 package com.example.haproslaunchapp;
 
+import android.graphics.Typeface;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import org.w3c.dom.Text;
 
 public class PageViewerAdapter extends FragmentStateAdapter {
 
@@ -13,24 +22,11 @@ public class PageViewerAdapter extends FragmentStateAdapter {
     @NonNull
     @Override
     public Fragment createFragment(int position){
-        switch(position){
-            case 0:
-                return new mediaPage(1);
-            case 1:
-                return new mediaPage(2);
-            case 2:
-                return new mediaPage(3);
-            case 3:
-                return new mediaPage(4);
-            case 4:
-                return new mediaPage(5);
-            case 5:
-                return new mediaPage(6);
-            case 6:
-                return new countDownFragment();
-            default:
-                return new countDownFragment();
-        }
+        if (position >= 6)
+            return new countDownFragment();
+        mediaPage frag = new mediaPage(position);
+
+        return frag;
     }
     @Override
     public int getItemCount(){return 7;}
--- app/src/main/java/com/example/haproslaunchapp/mediaPage.java
+++ app/src/main/java/com/example/haproslaunchapp/mediaPage.java
@@ -1,5 +1,6 @@
 package com.example.haproslaunchapp;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -11,10 +12,12 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.VideoView;
-//import org.json.simple.*;
-//import org.json.simple.parser.*;
+import org.json.simple.*;
+import org.json.simple.parser.*;
 import java.util.*;
 import java.io.*;
 import java.util.ArrayList;
@@ -31,7 +34,9 @@ public class mediaPage extends Fragment {
 
     int yearID;
     String launchDate, videoFile;
+    ScrollView scrollView;
 
+    ArrayList<Dictionary<String,String>> timeLineData;
     ArrayList<MediaPost> timeLineList;
 
 
@@ -40,41 +45,128 @@ public class mediaPage extends Fragment {
 
     public mediaPage(int yearID) {
         // Required empty public constructor
+
         yearID = yearID;
+        timeLineData = new ArrayList<>();
         timeLineList = new ArrayList<>();
-//        file = (Integer.toString(yearID)+"MediaInfo.json");
+//        timelineCreate();
+//        for (Dictionary<String,String> dict: timeLineData) {
+//            MediaPost post = new MediaPost(dict.get("imgDir"),dict.get("date"),dict.get("postDescription"));
+//            timeLineList.add(post);
+//
+//        }
+
+        System.out.println("Before Trying");
+//        file = loadJSONFromAsset();
 
 //        JSONParser parser = new JSONParser();
 //        try{
+//            System.out.println(file);
 //            Object obj = parser.parse(new FileReader(file));
+//            System.out.println("parsed");
+//
 //            JSONObject jsonObject = (JSONObject)obj;
 //            launchDate = (String)jsonObject.get("launchDate");
 //            videoFile = (String)jsonObject.get("videoDir");
 //            JSONArray jsonTimeline = (JSONArray)jsonObject.get("postTimeline");
+//            System.out.println("After getting objects");
 //            for (int i = 0; i < jsonTimeline.size(); i++) {
-//                String iImgDate = jsonTimeline.get(i).get("date");
-//                String iImgDir = jsonTimeline.get(i).get("imgDir");
-//                String iPostDesc = jsonTimeline.get(i).get("postDescription");
+//
+//                JSONObject postObj = (JSONObject) jsonTimeline.get(i);
+//
+//                String iImgDate = (String) postObj.get("date");
+//                String iImgDir = (String) postObj.get("imgDir");
+//                String iPostDesc = (String) postObj.get("postDescription");
 //                MediaPost x = new MediaPost(iImgDate,iImgDir,iPostDesc);
 //                timeLineList.add(x);
-//                }
-//        }catch(Exception e){
+//                System.out.println((iImgDate+iImgDir+iPostDesc));
+//
+//            }
+//            System.out.println("After for-loop");
+//
+//        }catch(Exception e) {
 //            System.out.print(e);
-//        if(timeLineList.size() > 0){
-//            for(MediaPost post:timeLineList){
+//            System.out.println("FAILED TO DO SOMETHING");
+//
+//        }
+//        if (timeLineList.size() > 0) {
+//            for (MediaPost post : timeLineList) {
 //                System.out.println(post.getImageDir());
 //                System.out.println(post.getPostDate());
 //                System.out.println(post.getPostDesc());
 //            }
 //        }
+//        System.out.println("bottom of constructor");
+
 
 
     }
+//    public String loadJSONFromAsset() {
+//        String json = null;
+//        try {
+//            System.out.println("Pre-iS");
+//            InputStream is = getResources().getAssets().open("json/0MediaInfo.json");
+//            System.out.println("after-iS");
+//
+//            int size = is.available();
+//            byte[] buffer = new byte[size];
+//            is.read(buffer);
+//            System.out.println("after read");
+//
+//            is.close();
+//            json = new String(buffer, "UTF-8");
+//            System.out.println("afterString");
+//
+//        } catch (IOException ex) {
+//            System.out.println("Pre-iS");
+//            ex.printStackTrace();
+//            return null;
+//        }
+//        return json;
+//    }
+    public ArrayList<MediaPost> getTimeLineList() {
+        return timeLineList;
+    }
+
+    private void timelineCreate(){
+        //Manually creating the data like this is temporary and will be changed later on
+        Dictionary<String,String>  x = new Hashtable<>();
+        x.put("date","May, 4, 2022");
+        x.put("imgDir","0504022.png");
+        x.put("postDescription","This is a small example post to use for each picture, this is where you will write a couple of sentences to describe the post.");
+        timeLineData.add(x);
+        Dictionary<String,String> z = new Hashtable<>();
+        z.put("date","May, 14, 2022");
+        z.put("imgDir","0514022.png");
+        z.put("postDescription","This is a small example post to use for each picture, this is where you will write a couple of sentences to describe the post.");
+        timeLineData.add(z);
+    }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_media_page, container, false);
+        View view =inflater.inflate(R.layout.fragment_media_page, container, false);
+        LinearLayout scrollView = (LinearLayout) view.findViewById(R.id.scroll_layout);
+        for (MediaPost post: getTimeLineList()) {
+            LinearLayout postLayout = new LinearLayout(view.getContext());
+            postLayout.setOrientation(LinearLayout.VERTICAL);
+            ImageButton postImg = new ImageButton(postLayout.getContext());
+            TextView dateHead = new TextView(postLayout.getContext());
+            TextView postDesc = new TextView(postLayout.getContext());
+            postImg.setScaleType(ImageView.ScaleType.FIT_CENTER);
+            postImg.setMaxWidth(182);
+//            postImg.setImageURI("Set Image Url");
+            postLayout.addView(postImg);
+            dateHead.setTextSize(24);
+            dateHead.setTypeface(dateHead.getTypeface(), Typeface.BOLD);
+            dateHead.setText(post.getPostDate());
+            postLayout.addView(dateHead);
+            postDesc.setTextSize(16);
+            postDesc.setText(post.getPostDesc());
+            postLayout.addView(postDesc);
+            scrollView.addView(postLayout);
+        }
+
+        return view;
     }
 }
