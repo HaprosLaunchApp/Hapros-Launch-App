fb9166129a12744483be3c2161b4fe137d7c8329
logan.lenoir <loganlenoir2006@gmail.com>
2023-05-12 08:13:03 -0600

    Logan Lenoir 5/12/2023
    Removed the home button in the about page, replacing it with a home button in the home page's actionbar.

--- app/src/main/java/com/example/haproslaunchapp/About_activity.java
+++ app/src/main/java/com/example/haproslaunchapp/About_activity.java
@@ -1,27 +1,44 @@
 package com.example.haproslaunchapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 
 public class About_activity extends AppCompatActivity {
 
-    ActionBar actionBar;
     Intent backHome;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
-        actionBar = getSupportActionBar();
-        actionBar.setTitle("About HAPROS");
+
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setHomeAsUpIndicator(R.drawable.tux_in_home2_resized);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        //Set text if needed.
+        //actionBar.setTitle("[Set Text Here]");
     }
 
     public void home(View view) {
         backHome = new Intent(About_activity.this, MainActivity.class);
         startActivity(backHome);
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
